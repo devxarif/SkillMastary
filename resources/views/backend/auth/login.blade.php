cdee4f783cac18855c2784b729ebe5790834fb41
Ariful Islam Arif <devboyarif@gmail.com>
2023-08-14 01:10:45 +0600

    backend resources added and auth page mastering

---
+++ resources/views/backend/auth/login.blade.php
@@ -0,0 +1,30 @@
+@extends('backend.layouts.auth')
+
+@section('auth-content')
+<div class="login-content">
+  <h3>Login to your account</h3>
+  <form action="#">
+    <div class="fromGroup rt-mb-16">
+      <label>Email</label>
+      <input class="form-control" type="email" placeholder="Email address" />
+    </div>
+    <div class="fromGroup eye rt-mb-24 forget-password">
+      <label>Password <a href="#">Forget Password</a></label>
+      <div class="form-control-icon">
+        <input id="password-hide_show" class="form-control" type="password" placeholder="password" />
+        <div class="has-badge">
+          <i class="ph-eye"></i>
+        </div>
+      </div>
+    </div>
+    <a class='btn btn-primary pill btn-icon d-block' href='/registration'>
+      <span class="button-content-wrapper">
+      <span class="button-icon align-icon-right">
+        <i class="ph-arrow-right"></i>
+      </span>
+      <span class="button-text">Login Now</span>
+      </span>
+    </a>
+  </form>
+</div>
+@endsection
