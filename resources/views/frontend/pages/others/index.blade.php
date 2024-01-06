3e6a29bf240b27beabc723e4ecab0dd41c00ae36
Ariful Islam Arif <devboyarif@gmail.com>
2024-01-07 00:59:55 +0600

    wip

--- resources/views/frontend/pages/others/index.blade.php
+++ resources/views/frontend/pages/others/index.blade.php
@@ -3,10 +3,6 @@
 @section('title', __('Home'))
 
 @section('content')
-<!--Hero Section -->
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
 <div class="hero-section">
     <div class="container">
         <div class="row st_screenfix">
@@ -412,38 +408,71 @@
             <div class="section-title section-title2">
                 <h2>Our featured instructor</h2>
             </div>
-            <div class="section-title section-title2">
-                <div class="feature-course-mid">
-                    <a href="#">
-                        <img src="http://127.0.0.1:8000/frontend/images/avatar.png" alt="user image" width="400px" height="400px">
-                        <h5>
-                            Mr. Instructor
-                        </h5>
-                    </a>
-                    <div>
-                        <div>
-                            <svg width="18" class="align-self-center" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.34462 13.401L13.2849 15.8974C13.7886 16.2165 14.4139 15.7419 14.2644 15.154L13.126 10.6756C13.0939 10.5509 13.0977 10.4197 13.137 10.297C13.1762 10.1743 13.2492 10.0652 13.3477 9.98222L16.8811 7.04132C17.3453 6.6549 17.1057 5.88439 16.5092 5.84567L11.8949 5.5462C11.7706 5.53732 11.6514 5.49332 11.5511 5.41931C11.4509 5.34531 11.3737 5.24435 11.3286 5.12819L9.60765 0.794357C9.56087 0.671064 9.47769 0.564919 9.36915 0.490017C9.26062 0.415115 9.13187 0.375 9 0.375C8.86813 0.375 8.73938 0.415115 8.63085 0.490017C8.52232 0.564919 8.43914 0.671064 8.39236 0.794357L6.6714 5.12819C6.62631 5.24435 6.54914 5.34531 6.4489 5.41931C6.34865 5.49332 6.22944 5.53732 6.10515 5.5462L1.49078 5.84567C0.894294 5.88439 0.654664 6.6549 1.11894 7.04132L4.65232 9.98222C4.75079 10.0652 4.82383 10.1743 4.86305 10.297C4.90226 10.4197 4.90606 10.5509 4.874 10.6756L3.81824 14.8288C3.63889 15.5343 4.38929 16.1038 4.99369 15.7209L8.65539 13.401C8.75837 13.3354 8.87792 13.3006 9 13.3006C9.12208 13.3006 9.24163 13.3354 9.34462 13.401Z" fill="#FD8E1F"></path>
-                            </svg>
-                            <p>
-                                0<span>(468.6k)</span>
-                            </p>
+            <div class="mb-5">
+                <div class="tw-max-w-3xl md:tw-flex md:tw-items-center md:tw-justify-between md:tw-space-x-5 lg:tw-max-w-7xl">
+                    <div class="tw-flex tw-items-center tw-space-x-5">
+                      <div class="tw-flex-shrink-0">
+                        <div class="tw-relative">
+                          <img class="tw-h-20 tw-w-20 tw-rounded-full" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=1024&amp;h=1024&amp;q=80" alt="">
+                          <span class="tw-absolute tw-inset-0 tw-rounded-full tw-shadow-inner" aria-hidden="true"></span>
                         </div>
+                      </div>
+                      <div>
+                        <h1 class="tw-text-2xl tw-font-bold tw-text-gray-900">Ricardo Cooper</h1>
+                        <p class="tw-text-sm tw-font-medium tw-text-gray-500">Learning since <time datetime="2020-08-25">August 25, 2020</time></p>
+                        <div class="tw-flex tw-justify-start profile-review mt-3">
+                            <ul class="tw-flex tw-gap-3">
+                              <li>
+                                <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M10.4135 15.8812L15.1419 18.8769C15.7463 19.2598 16.4967 18.6903 16.3173 17.9847L14.9512 12.6108C14.9127 12.4611 14.9173 12.3036 14.9643 12.1564C15.0114 12.0092 15.0991 11.8783 15.2172 11.7787L19.4573 8.24959C20.0144 7.78588 19.7269 6.86126 19.0111 6.81481L13.4738 6.45544C13.3247 6.44479 13.1816 6.39198 13.0613 6.30317C12.941 6.21437 12.8484 6.09321 12.7943 5.95382L10.7292 0.753229C10.673 0.605277 10.5732 0.477903 10.443 0.38802C10.3127 0.298137 10.1582 0.25 10 0.25C9.84176 0.25 9.68726 0.298137 9.55702 0.38802C9.42678 0.477903 9.32697 0.605277 9.27083 0.753229L7.20568 5.95382C7.15157 6.09321 7.05897 6.21437 6.93868 6.30317C6.81838 6.39198 6.67533 6.44479 6.52618 6.45544L0.98894 6.81481C0.273153 6.86126 -0.0144031 7.78588 0.542723 8.24959L4.78278 11.7787C4.90095 11.8783 4.9886 12.0092 5.03566 12.1564C5.08272 12.3036 5.08727 12.4611 5.0488 12.6108L3.78188 17.5945C3.56667 18.4412 4.46715 19.1246 5.19243 18.6651L9.58647 15.8812C9.71005 15.8025 9.85351 15.7607 10 15.7607C10.1465 15.7607 10.29 15.8025 10.4135 15.8812Z" fill="#FD8E1F"></path>
+                                </svg>
+                                <span>4.8</span>
+                                (134,633 review)
+                              </li>
+                              <li>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M8.25 15C10.9424 15 13.125 12.8174 13.125 10.125C13.125 7.43261 10.9424 5.25 8.25 5.25C5.55761 5.25 3.375 7.43261 3.375 10.125C3.375 12.8174 5.55761 15 8.25 15Z" stroke="#564FFD" stroke-width="1.5" stroke-miterlimit="10"></path>
+                                  <path d="M14.5698 5.43158C15.2403 5.24266 15.9436 5.19962 16.6321 5.30537C17.3207 5.41111 17.9786 5.66318 18.5615 6.04459C19.1444 6.42601 19.6389 6.92791 20.0115 7.5165C20.3841 8.10509 20.6263 8.7667 20.7217 9.45676C20.8171 10.1468 20.7635 10.8493 20.5645 11.5169C20.3655 12.1845 20.0258 12.8018 19.5682 13.327C19.1107 13.8523 18.5458 14.2734 17.9118 14.562C17.2777 14.8505 16.5892 14.9999 15.8926 15" stroke="#564FFD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                  <path d="M1.49951 18.5059C2.26089 17.4229 3.27166 16.539 4.4465 15.9288C5.62133 15.3186 6.92574 15.0001 8.24959 15C9.57344 14.9999 10.8779 15.3184 12.0528 15.9285C13.2276 16.5386 14.2385 17.4225 14.9999 18.5054" stroke="#564FFD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                  <path d="M15.8926 15C17.2166 14.999 18.5213 15.3171 19.6962 15.9273C20.8712 16.5375 21.8819 17.4218 22.6426 18.5054" stroke="#564FFD" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <span>430,117</span>
+                                students
+                              </li>
+                              <li>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" fill="#FF6636" stroke="#FF6636" stroke-width="1.5" stroke-miterlimit="10"></path>
+                                  <path d="M15 12L10.5 9V15L15 12Z" fill="white" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <span>7</span>
+                                courses
+                              </li>
+                            </ul>
+                          </div>
+                      </div>
                     </div>
-                </div>
+                    <a class="button button--lg button-p-s button-i--r" href="{{ route('website.course') }}">View Profile
+                        <span>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.75 12H20.25" stroke="#FF6636" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                                <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="#FF6636" stroke-width="1.5"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </span>
+                    </a>
 
-                <p>Vestibulum sed dolor sed diam mollis maximus vel nec dolor.<br /> Donec varius purus et eleifend
-                    porta.</p>
+                  </div>
             </div>
             <div class="row">
-                @foreach ($featuredCourses as $course)
+                {{-- @foreach ($featuredCourses as $course)
                     <div class="col-xl-6">
                         <x-frontend.course.width-card :course="$course" />
                     </div>
-                @endforeach
+                @endforeach --}}
             </div>
             <div class="footer-button text-center mt-3">
-                <a class="button button--lg button-p-s button-i--r" href="http://127.0.0.1:8000/courses">View all featured courses
+                <a class="button button--lg button-p-s button-i--r" href="http://127.0.0.1:8000/courses">View all featured instructor
                     <span>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.75 12H20.25" stroke="#FF6636" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -535,24 +564,7 @@
             </div>
             <div class="course-item-5-grid">
                 <div class="course-item-5">
-                    <div class="mx-auto max-w-3xl px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
-                        <div class="flex items-center space-x-5">
-                          <div class="flex-shrink-0">
-                            <div class="relative">
-                              <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=1024&amp;h=1024&amp;q=80" alt="">
-                              <span class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></span>
-                            </div>
-                          </div>
-                          <div>
-                            <h1 class="text-2xl font-bold text-gray-900">Ricardo Cooper</h1>
-                            <p class="text-sm font-medium text-gray-500">Applied for <a href="#" class="text-gray-900">Front End Developer</a> on <time datetime="2020-08-25">August 25, 2020</time></p>
-                          </div>
-                        </div>
-                        <div class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-                          <button type="button" class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Disqualify</button>
-                          <button type="button" class="inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Advance to offer</button>
-                        </div>
-                      </div>
+
                     <div class="course-item-5-card">
                         <a href="instructor-profile.html" class="course-item-5-card-img team-thumb"><img
                                 src="{{ asset('frontend') }}/images/team1.png" alt=""></a>
