f60278f5b5433091c81b8f9b87d6d37731366e1a
Ariful Islam Arif <devboyarif@gmail.com>
2024-01-25 21:35:04 +0600

    wip

--- resources/views/livewire/frontend/auth/register.blade.php
+++ resources/views/livewire/frontend/auth/register.blade.php
@@ -100,16 +100,14 @@
                         {{ __('I Agree with all of your') }} <a href="#">Terms &amp; Conditions</a>
                     </label>
                 </div>
-                <button type="submit" class="button button--md button-i--r">
+                <button wire:loading.attr="disabled" type="submit" class="button button--md button-i--r">
                     {{ __('Create Account') }}
-                    <span>
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3.75 12H20.25" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                                stroke-linejoin="round"></path>
-                            <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="white" stroke-width="1.5"
-                                stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
+
+                    <span wire:loading.remove wire:target="register">
+                        <x-svg.arrow-right-icon />
+                    </span>
+                    <span wire:loading wire:target="register">
+                        <x-svg.loading-icon />
                     </span>
                 </button>
             </div>
